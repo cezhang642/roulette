659d5b0db776add2765a26c129cec3d4d2659bf1
Christina Zhang <christinazhang@Brians-MBP.fios-router.home>
2021-01-20 14:51:30 -0500

    added beginning randomizer

--- yelp-roulette/src/Search.js
+++ yelp-roulette/src/Search.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
+import axios from 'axios';
 import './Search.css';
 class Search extends Component {
     state = {
         searchValue: '',
-        meals: []
+        items : [],
+        curr : []
     };
     handleOnChange = event => {
         this.setState({ searchValue: event.target.value });
@@ -11,18 +13,33 @@ class Search extends Component {
     handleSearch = () => {
         this.makeApiCall(this.state.searchValue);
     }
-    makeApiCall = searchInput => {
-        var searchUrl = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInput}`;
-        fetch(searchUrl)
-        .then(response => {
-        return response.json();
+    makeApiCall = async searchInput => {
+        const BEARER_TOKEN = '2oXsdHbjPVgyM7xmdOXjt3gBquPiu9ZqWG6ww-gAd5SGKUrJ4gscvvO4yy5tKF9PMtOrw0g6XIQKb8t9IftBw-VMEWxNPReLG0P6jVAsSN8xbre5W13g3tic5Kr8X3Yx';
+        const data = await axios.get(
+          `${'https://cors-anywhere.herokuapp.com/'}https://api.yelp.com/v3/businesses/search?location=usa`,
+          {
+            headers: {
+              Authorization: `Bearer ${BEARER_TOKEN}`,
+            },
+            params: {
+              term: searchInput,
+            },
+          },
+        )
+
+        .then(json => {
+            this.setState({ items: json.data.businesses });
         })
-        .then(jsonData => {
-        console.log(jsonData.meals);
-        this.setState({ meals: jsonData.meals });
+        .then(resty => {
+          this.setState({ curr: this.state.items[Math.floor(Math.random() * this.state.items.length)]})
+        })
+        .catch(err => {
+            console.log(err);
         });
     };
+
     render() {
+      const k = Math.floor(Math.random() *(this.state.items).length)
         return (
             <div>
             <h1>Welcome to the Restaurant Roulette</h1>
@@ -34,12 +51,22 @@ class Search extends Component {
             value={this.state.searchValue}
             />
             <button onClick={this.handleSearch}>Roll!</button>
-            {this.state.meals ? (
+            {console.log(this.state.curr)}
+            {this.state.items ? (
+            //   <div>
+            //   {
+            //   <h1>{((this.state.items)[k]).name}</h1>
+            //   // <img src={this.state.items[k].image_url} alt="meal-thumbnail" />
+            //   // </div>
+            //   // ))}
+            // }
+            //   </div>
             <div>
-            {this.state.meals.map((meal, index) => (
+              <h1>{this.state.curr.name}</h1>
+            {this.state.items.map((businesses, index) => (
             <div key={index}>
-            <h1>{meal.strMeal}</h1>
-            <img src={meal.strMealThumb} alt="meal-thumbnail" />
+            <h1>{businesses.name}</h1>
+            <img src={businesses.image_url} alt="meal-thumbnail" />
             </div>
             ))}
             </div>
